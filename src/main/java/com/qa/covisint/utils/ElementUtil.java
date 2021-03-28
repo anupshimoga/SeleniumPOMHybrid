54e343181f46b95cbb4f816c56fa75696e5d061d
Anup Shimoga <anups141988@gmail.com>
2021-03-29 00:49:40 +0530

    Lambda removed

--- src/main/java/com/qa/covisint/utils/ElementUtil.java
+++ src/main/java/com/qa/covisint/utils/ElementUtil.java
@@ -152,9 +152,9 @@
 			return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
 		}
 
-		public void getPageLinksText(By locator, int timeOut) {
-			visibilityOfAllElements(locator, timeOut).stream().forEach(ele -> System.out.println(ele.getText()));
-		}
+//		public void getPageLinksText(By locator, int timeOut) {
+//			visibilityOfAllElements(locator, timeOut).stream().forEach(ele -> System.out.println(ele.getText()));
+//		}
 
 		public int getPageLinksCount(By locator, int timeOut) {
 			return visibilityOfAllElements(locator, timeOut).size();
